556ad2598c3fa8e90899521c0788671966e24044
hreshi <hkale746@gmail.com>
2021-10-15 10:30:55 +0530

    removed unused constructors and setters

--- src/main/java/com/aissms/groupchat/Message.java
+++ src/main/java/com/aissms/groupchat/Message.java
@@ -4,23 +4,12 @@ public class Message{
 	private String name;
 	private String message;
 	
-	Message() {
-	}
 	Message(String message, String name) {
 		this.message = message;
 		this.name = name;
 	}
-	
-	// getters and setters methods
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 	public String getMessage() {
 		return message;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 	public String getName() {
 		return name;
